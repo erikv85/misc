c22b14d38eb97f047b050369266826827982e28d
Erik Vesterlund <erik.vesterlund@gmail.com>
2021-06-10 19:24:09 +0200

    vagrant

---
+++ vagrant-stuff/README.txt
@@ -0,0 +1,5 @@
+Basic vagrant.
+
+vagrant up
+vagrant ssh -c "npm --prefix hello-world run serve"
+go to localhost:5000
---
+++ vagrant-stuff/provision.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g @vue/cli
+vue create -d hello-world
